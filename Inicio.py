19e19ec9ed12c8382d50e0eb783f309df4224198
jglimon <jgilberto_mty@hotmail.com>
2023-08-17 22:08:00 -0600

    Update Inicio.py

--- Inicio.py
+++ Inicio.py
@@ -1,4 +1,8 @@
 import streamlit as st
 
 st.title('Convertidor de Estados de Cuenta PDF a Excel')
+
+# Python logo image
+
+st.image("IEM_Logo.png", width=300)
 st.text('Podrás fácilmente convertir los Estados de Cuenta de diferentes bancos a un archivo Excel (.csv) de manera rápida.')
