b7f2ed061572a573ce34220cdfb92b71e6c0f62c
jglimon <jgilberto_mty@hotmail.com>
2023-08-17 21:10:57 -0600

    Delete Inicio.py

--- Inicio.py
+++
@@ -1,2 +0,0 @@
-st.title('Convertidor de Estados de Cuenta PDF a Excel')
-st.text('Podrás fácilmente convertir los Estados de Cuenta de diferentes bancos a un archivo Excel (.csv) de manera rápida.')
